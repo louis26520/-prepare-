18c23fc12944d96492ff8723141eb586d898de34
Louis <louis@Louisde-MacBook-Air.local>
2017-08-30 14:47:06 +0800

    Initial Commit

---
+++ 四大公開賽 頁面資料傳輸/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  四大公開賽 頁面資料傳輸
+//
+//  Created by Louis on 2017/8/30.
+//  Copyright © 2017年 Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
